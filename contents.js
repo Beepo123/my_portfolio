6c46a3593f256dd31ef030eed080cba7e4d9cb17
samuel <samuelgipit@gmail.com>
2024-03-18 11:21:50 +0800

    made projects icon interactive

--- contents.js
+++ contents.js
@@ -1,21 +1,29 @@
 const projects = [
-  ["calculator-icon", "calculator"],
-  ["password-generator-icon", "password generator"],
-  ["quiz-icon", "quiz game"],
-  ["quote-generator-icon", "quote generator"],
-  ["todo-list-icon", "todo list"],
-  ["validation-form-icon", "validation form"],
-  ["weather-app-icon", "weather app"],
+  ["calculator/calculator.html", "calculator-icon", "calculator"],
+  [
+    "password-generator/password-generator.html",
+    "password-generator-icon",
+    "password generator",
+  ],
+  ["quiz/quiz.html", "quiz-icon", "quiz game"],
+  ["quote-generator/quote.html", "quote-generator-icon", "quote generator"],
+  ["todo-list-app/todo-list.html", "todo-list-icon", "todo list"],
+  ["validation-form/index.html", "validation-form-icon", "validation form"],
+  ["weather-app/weather-app.html", "weather-app-icon", "weather app"],
 ];
+
+const projectLinks = [];
 
 export function renderProjects() {
   let projectsHTML = ``;
   projects.forEach((project) => {
     const html = `
-      <div class="project-container">
-        <div class="project-thumbnail"><img src="images/${project[0]}.png"></div>
-        <div class="project-title">${project[1]}</div>
-      </div>
+      <a href="mini-js-projects-main/${project[0]}">
+        <div class="project-container">
+          <div class="project-thumbnail"><img src="images/${project[1]}.png"></div>
+          <div class="project-title">${project[2]}</div>
+        </div>
+      </a>
     `;
     projectsHTML += html;
   });
